c73ac58ab37c45e7a5aeb0abae364f1ac623169e
杨剑锋 <331387202@qq.com>
2017-10-30 01:35:15 +0800

    Done.

--- demo/Test.py
+++ demo/Test.py
@@ -4,3 +4,21 @@ Created on 2017年10月20日
 
 @author: Jeff Yang
 '''
+import requests
+from urllib.request import urlretrieve
+# import json
+
+url = "http://h.bilibili.com/wallpaperApi?action=getOptions&page=1"
+html = requests.get(url)
+# print(type(html))
+# content = json.loads(html.text)
+content = html.json()
+
+path = 'D:/Wallpaper/'
+i = 1
+while i <= len(content) - 1:
+    src = content[i]['detail'][0]['il_file']
+    print('Downloading ' + content[i]['detail'][0]['title'] + ' ……')
+    urlretrieve(src, path + str(i) + src[-4:])
+    i = i + 1
+print('Done!')
